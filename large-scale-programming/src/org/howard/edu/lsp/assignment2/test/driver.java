686c4a7d09cd75eccc0d5e12585a9279f393aca5
a_duck_named_bostic <55213951+bosticc@users.noreply.github.com>
2021-02-11 17:30:34 -0500

    add driver class; remove alltests.java

---
+++ large-scale-programming/src/org/howard/edu/lsp/assignment2/test/driver.java
@@ -0,0 +1,81 @@
+/**
+ * 
+ */
+package org.howard.edu.lsp.assignment2.test;
+
+import java.util.ArrayList;
+
+import org.howard.edu.lsp.assignment2.combination.Solution;
+/**
+ * @author bostic
+ *
+ */
+public class driver {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		allOnes();
+		addToTen();
+		zeroTarget();
+		addTo3();
+		
+	}
+		/**
+		 * Tests if the input has all ones will the correct output work
+		 */
+		public static void allOnes() {
+			int[] allOnes = {1,1,1,1};
+			int target = 1;
+		    ArrayList<Integer> allIndexes = new ArrayList<Integer>();
+		    allIndexes.add(0);
+		    allIndexes.add(1);
+		    allIndexes.add(2);
+		    allIndexes.add(3);
+
+			Solution test = new Solution(allOnes, target);
+		}
+		
+		/**
+		 * Tests if the input has a target of will the correct output work
+		 */
+		public static void addToTen() {
+			int[] addTo10 = {99,28,43903, 10};
+			int target = 10;
+			
+		    ArrayList<Integer> allIndexes = new ArrayList<Integer>();
+		    allIndexes.add(3);
+			Solution test = new Solution(addTo10, target);
+		}
+		
+		/**
+		 * Tests if the input has a empty array will the correct output work
+		 */
+		public static void zeroTarget() {
+			int[] addTo0 = {};
+			int target = 0;
+			
+		    ArrayList<Integer> allIndexes = new ArrayList<Integer>();
+			Solution test = new Solution(addTo0, target);
+		}
+		
+		/**
+		 * Tests if target adds up to 3 will the output work
+		 */
+		public static void addTo3()
+		{
+			int[] addTo15 = {1,2,3,4,5,6};
+			int target = 3;
+			
+		    ArrayList<Integer> allIndexes = new ArrayList<Integer>();
+		    allIndexes.add(0);
+		    allIndexes.add(1);
+		    allIndexes.add(2);
+		    
+			Solution test = new Solution(addTo15, target);
+		}
+
+	}
